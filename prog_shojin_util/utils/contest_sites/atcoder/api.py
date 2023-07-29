184867eec9cdb922f8f6ead52c4915c254538394
edge2992 <torune.summer@gmail.com>
2023-07-29 13:04:58 +0900

    feat(api.py): add caching mechanism to read and write data to cache for faster retrieval of user submissions

--- prog_shojin_util/utils/contest_sites/atcoder/api.py
+++ prog_shojin_util/utils/contest_sites/atcoder/api.py
@@ -1,24 +1,64 @@
+import hashlib
+import json
 import time
 import requests
 import logging
+import os
 
 from ..abstract import APIInterface
 
 BASE_URL = "https://kenkoooo.com/atcoder/atcoder-api/v3"
+CACHE_DIR = os.path.expanduser("~/.cache/prog-shojin-util")
 logger = logging.getLogger(__name__)
 
 
 class AtcoderAPI(APIInterface):
     SUBMISSION_LIMIT = 500
 
+    def _read_from_cache(self, user_id: str, from_second: int):
+        cache_file = self._get_cache_filename(user_id, from_second)
+        if os.path.exists(cache_file):
+            logger.debug(
+                f"Reading cached data for user {user_id} from {cache_file}."
+            )
+            with open(cache_file, "r") as f:
+                return json.load(f)
+        logger.debug(f"No cache found for user {user_id} at {cache_file}.")
+        return None
+
+    def _write_to_cache(self, user_id: str, from_second: int, data: list):
+        if not os.path.exists(CACHE_DIR):
+            os.makedirs(CACHE_DIR)
+            logger.debug(
+                f"Cache directory {CACHE_DIR} not found. Created new one."
+            )
+        cache_file = self._get_cache_filename(user_id, from_second)
+        with open(cache_file, "w") as f:
+            json.dump(data, f)
+        logger.debug(
+            f"Saved data for user {user_id} to cache at {cache_file}."
+        )
+
+    @staticmethod
+    def _get_cache_filename(user_id: str, from_second: int) -> str:
+        hashed_key = hashlib.md5(
+            f"{user_id}_{from_second}".encode()
+        ).hexdigest()
+        return os.path.join(CACHE_DIR, f"{hashed_key}.json")
+
     def _fetch_submissions(self, user_id: str, from_second: int) -> list[dict]:
+        iter_second = from_second
+        cached_data = self._read_from_cache(user_id, from_second)
+        if cached_data:
+            return cached_data
+
         all_submissions = []
 
         while True:
             endpoint = f"{BASE_URL}/user/submissions"
-            params = {"user": user_id, "from_second": from_second}
+            params = {"user": user_id, "from_second": iter_second}
             logger.debug(
-                f"Fetching submissions for user {user_id} from {from_second}"
+                f"Fetching submissions for user {user_id} from {iter_second}"
             )
 
             response = requests.get(endpoint, params=params)
@@ -34,9 +74,10 @@ class AtcoderAPI(APIInterface):
                 logger.debug("No more submissions to fetch")
                 break
 
-            from_second = submissions[-1]["epoch_second"] - 1
+            iter_second = submissions[-1]["epoch_second"] - 1
             time.sleep(1)
 
+        self._write_to_cache(user_id, from_second, all_submissions)
         return all_submissions
 
     def _filter_ac_problems(self, submissions: list[dict]) -> list[dict]:
